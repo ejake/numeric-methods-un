0dda9d23933c28e84d1a8a6e330f364cf02daeae
andres.jaque <andres.jaque@gmail.com>
2012-02-28 13:44:46 +0000

    Adding two hills

--- matlab/test4mesh.m
+++ matlab/test4mesh.m
@@ -6,6 +6,8 @@ xb = -2:0.1:2;
 % z = cos(x.^2) + sin(y.^2);
 %z = exp(-x.^2 - y.^2);
 z = exp(-x.^2 - y.^2);
+z1 = exp(-(x+1).^2 - (y+1).^2);
+z2 = z+z1;
 meshc(x,y,z) % surface in x
 %mesh(x,y,z)
 % view(60,30)

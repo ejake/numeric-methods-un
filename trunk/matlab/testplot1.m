db86999b3f917e1faf61d301090645a562074464
andres.jaque@gmail.com <andres.jaque@gmail.com@d110e4a1-fc1a-e020-64b5-30de8687fcda>
2012-02-28 12:51:06 +0000

    simple plot example

---
+++ trunk/matlab/testplot1.m
@@ -0,0 +1,12 @@
+clear all;
+close all;
+
+x= 0:0.05:5;
+y1=x.^2;
+y2=x.^3;
+y3=x.^3;
+y4=x.^4;
+plot(x,y1,'-r',x,y2,'*k',x,y3,'.m',x,y4,'-.b')
+legend('f(x)=x','f(x)=x^2', 'f(x)=x^3', 'f(x)=x^4')
+title('Second figure x^n n \in (2,4)')v
+

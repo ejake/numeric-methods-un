7b195715d83bd353418c120baaeffdb954f79d36
andres.jaque <andres.jaque@gmail.com>
2012-02-28 12:53:50 +0000

    Adding labels

--- matlab/testplot1.m
+++ matlab/testplot1.m
@@ -7,6 +7,9 @@ y2=x.^3;
 y3=x.^3;
 y4=x.^4;
 plot(x,y1,'-r',x,y2,'*k',x,y3,'.m',x,y4,'-.b')
+xlabel('x')
+ylabel('f(x)')
 legend('f(x)=x','f(x)=x^2', 'f(x)=x^3', 'f(x)=x^4')
-title('Second figure x^n n \in (2,4)')v
+title('Second figure x^n n \in (2,4)')
+
 

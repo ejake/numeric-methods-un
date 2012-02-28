5d0f81198c6118e4ba72294afb9186c7705e5112
andres.jaque <andres.jaque@gmail.com>
2012-02-28 13:42:58 +0000

    Drawing surfaces using mesh

---
+++ matlab/test4mesh.m
@@ -0,0 +1,11 @@
+% Plot surface using mesh
+
+xa = -2:0.1:2;
+xb = -2:0.1:2;
+[x,y] = meshgrid(xa,xb);
+% z = cos(x.^2) + sin(y.^2);
+%z = exp(-x.^2 - y.^2);
+z = exp(-x.^2 - y.^2);
+meshc(x,y,z) % surface in x
+%mesh(x,y,z)
+% view(60,30)

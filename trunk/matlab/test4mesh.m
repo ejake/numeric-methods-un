1f7a4435b5888df97fec24b3d95927c98a61ed7e
andres.jaque@gmail.com <andres.jaque@gmail.com@d110e4a1-fc1a-e020-64b5-30de8687fcda>
2012-02-28 13:54:24 +0000

    Adding animation plot

--- trunk/matlab/test4mesh.m
+++ trunk/matlab/test4mesh.m
@@ -5,9 +5,15 @@ xb = -2:0.1:2;
 [x,y] = meshgrid(xa,xb);
 % z = cos(x.^2) + sin(y.^2);
 %z = exp(-x.^2 - y.^2);
-z = exp(-x.^2 - y.^2);
-z1 = exp(-(x+1).^2 - (y+1).^2);
-z = z+z1;
+z1 = exp(-x.^2 - y.^2);
+z2 = -exp(-(x+1).^2 - (y+1).^2);
+z = z1+z2;
 meshc(x,y,z) % surface in x
 %mesh(x,y,z)
 % view(60,30)
+% animate plot:
+for i=1:10:360
+    meshc(x,y,z)
+    view(30,i)
+    pause(2)
+end
